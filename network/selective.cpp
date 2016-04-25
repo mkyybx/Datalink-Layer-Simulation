3b9d4ad3ea11905139708b26abd620239e91f96c
mkyybx <mky111@outlook.com>
2016-04-25 23:10:14 +0800

    new

--- network/selective.cpp
+++ network/selective.cpp
@@ -3,13 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 #define MAX_PACKET_SIZE 256
-
+//#define debug
+//#define windowdebug
 //what if crc doesn't found error? should we do sth?
 
 struct buffer {
 	unsigned char data[MAX_PACKET_SIZE + 7];
-	bool ackArrived;//for receiver, it means if a frame has arrived
+	bool frameArrived;//for receiver, it means if a frame has arrived
+	bool hasSent;//if the frame has sent in sender
 	int length;
 };
 
@@ -23,43 +26,85 @@ unsigned short senderLeft;//left edge of sender
 unsigned short senderRight;//right edge of sender, which has data unfilled
 unsigned short receiverLeft;//left edge of receiver
 //unsigned short receiverRight = bufferSize - 1;//right edge of receiver
+short isPhysicalLayerReady;//-1 means not initiated, 0 means no, 1 means yes
+short lastAck;//the last ack synced
+
 //init buffer
 buffer* sender;
 buffer* receiver;
 
 void mySendFrame(unsigned char* databuff, int size) {//add crc and cooperate with physical layer. databuff should be ready to send. size shoule be without crc
-	//append crc
-	unsigned char *p = databuff + size;
-	*(unsigned int*)p = crc32(databuff, size);
-	size += 4;//add length
 
-	if (databuff[0] == FRAME_DATA) {
-		//add timer
-		start_timer(databuff[1] % bufferSize, retimer);
+	if (databuff[0] != FRAME_NAK) {
 		//piggyback ack
 		short i = (receiverLeft - 1) % windowSize;//log the last ack arrived
-		for (short j = receiverLeft, k = 0; k < bufferSize; k++) {
-			receiver[j % bufferSize].ackArrived ? i = j % windowSize : 0;
-			j++;
+		if (i < 0)
+			i += windowSize;
+		for (short j = 0; j < bufferSize; j++) {
+			if (receiver[(receiverLeft + j) % bufferSize].frameArrived)
+				i = (receiverLeft + j) % windowSize;
+			else break;
 		}
-		databuff[2] = i;
+		databuff[0] == FRAME_DATA ? databuff[2] = i : databuff[1] = i;
 	}
 
-	while (phl_sq_len() >= 62000) //physical layer is not ready
-		_sleep(10);
+	//append crc
+	*(unsigned int*)(databuff + size) = crc32(databuff, size);
+	size += 4;//add length
+
+	if (databuff[0] == FRAME_DATA)
+		//add timer
+		start_timer(databuff[1] % bufferSize, retimer);
+#ifdef debug
+	printf("send:\t");
+	if (databuff[0] == FRAME_ACK)
+		printf("ack=%d", databuff[1]);
+	else if (databuff[0] == FRAME_NAK)
+		printf("nak=%d", databuff[1]);
+	else {
+		printf("seq=%d,ack=%d,datano=%d", databuff[1], databuff[2], *(short*)(databuff + 3));
+	}
+	printf("\n");
+#endif
+
+	//if (phl_sq_len() >= 1000) {//physical layer is not ready
+		//Sleep(200);
+	//	printf("%d\n", phl_sq_len());
+	//}
+
+	//printf("%d\n", phl_sq_len());
+
+
+	dbg_event("send");
+	dbg_event("kind=%d", databuff[0]);
+	if (databuff[0] != FRAME_DATA) {
+		dbg_event("ack/nak=%d", databuff[1]);
+		dbg_event("\n");
+	}
+	else {
+		dbg_event("seq=%d,ack=%d", databuff[1], databuff[2]);
+		dbg_event(",data=\n");
+		for (int i = 3; i <= size - 4; i++)
+			dbg_event("%d ", databuff[i]);
+		dbg_event("\n");
+	}
+	
 	send_frame(databuff, size);
+	
 
 	//piggyback ack timer
-	if (databuff[0] == FRAME_DATA) {//restart timer
+	if (databuff[0] != FRAME_NAK) {//restart timer
 		stop_ack_timer();
 		start_ack_timer(acktimer);
 	}
 
 }
 
-bool isInBuffer(short left, short right, short seq) {//whether a serial number is in buffer range,[left, right)
+bool isInBuffer(short left, short right, short seq, bool isReceiver) {//whether a serial number is in buffer range,[left, right)
 	if (right > left)
 		return (seq >= left && seq < right);
+	else if (left == right)
+		return false;
 	else return seq < right || seq >= left;
 }
 
@@ -68,20 +113,24 @@ void main(int argc, char** argv) {
 	protocol_init(argc, argv);
 	//arguments
 	windowSize = 8;//from 0 to n-1
-	retimer = 800;//for debuging, it should be large enough
-	acktimer = 100;
+	retimer = 520;//for debuging, it should be large enough
+	acktimer = 1000;
 	bufferSize = windowSize / 2;//buffer size
 	//init datalink layer
 	senderLeft = 0;//left edge of sender
 	senderRight = 0;//right edge of sender, which has data unfilled
 	receiverLeft = 0;//left edge of receiver
 	//receiverRight = bufferSize - 1;//right edge of receiver
+	isPhysicalLayerReady = -1;
+	lastAck = windowSize - 1;
 	//init buffer
 	sender = (buffer*)malloc(sizeof(buffer)* bufferSize);
 	receiver = (buffer*)malloc(sizeof(buffer)* bufferSize);
 	for (int i = 0; i < bufferSize; i++) {
-		sender[i].ackArrived = false;
-		receiver[i].ackArrived = false;
+		sender[i].frameArrived = false;
+		receiver[i].frameArrived = false;
+		sender[i].hasSent = false;
+		receiver[i].hasSent = false;
 	}
 	//init interfcace
 	enable_network_layer();
@@ -91,11 +140,20 @@ void main(int argc, char** argv) {
 	int eventKind = -1;
 	//has nak sent
 	bool isNakSend = false;
+	//alloc temp space
+	unsigned char temp[MAX_PACKET_SIZE + 11];
 	//main loop
 	while (true) {
+		//printf("%d\n", phl_sq_len());
+		static int frameLength;
 		eventKind = wait_for_event(&eventArgs);//get event
-		/*we discard PHYSICAL_LAYER_READY events*/
+#ifdef debug
+		printf("framekind=%d\n", eventKind);
+#endif
 		switch (eventKind) {
+		case PHYSICAL_LAYER_READY:
+			isPhysicalLayerReady = 1;
+			break;
 		case NETWORK_LAYER_READY:
 			//if buffer nearly full
 			if (((senderRight > senderLeft) && (senderRight - senderLeft == bufferSize - 1)) || (senderRight < senderLeft) && (windowSize - senderLeft + senderRight == bufferSize - 1)) {
@@ -104,46 +162,157 @@ void main(int argc, char** argv) {
 			}
 			//store frame in buffer
 			sender[senderRight % bufferSize].length = get_packet(sender[senderRight % bufferSize].data);
+			
+			//slide window
+			senderRight = (senderRight + 1) % windowSize;
+
 			break;
 		case FRAME_RECEIVED:
 			//init temp vars
-			static unsigned char temp[MAX_PACKET_SIZE + 11];
-			static int frameLength = recv_frame(temp, MAX_PACKET_SIZE + 7);
+			frameLength = recv_frame(temp, MAX_PACKET_SIZE + 7);
+#ifdef debug
+			printf("receive:");
+			if (temp[0] == FRAME_ACK)
+				printf("ack=%d", temp[1]);
+			else if (temp[0] == FRAME_NAK)
+				printf("nak=%d", temp[1]);
+			else {
+				printf("seq=%d,ack=%d,datano=%d", temp[1], temp[2], *(short*)(temp + 3));
+			}
+			printf("\n");
+#endif
+			dbg_event("receive:");
+			dbg_event("kind=%d", temp[0]);
+			if (temp[0] != FRAME_DATA) {
+				dbg_event("ack/nak=%d", temp[1]);
+				dbg_event("\n");
+			}
+			else {
+				dbg_event("seq=%d,ack=%d", temp[1], temp[2]);
+				dbg_event(",data=\n");
+				for (int i = 3; i <= frameLength - 4; i++)
+					dbg_event("%d ", temp[i]);
+				dbg_event("\n");
+			}
+
 			if (frameLength > MAX_PACKET_SIZE + 7)
 				;//frame is too large, discard it
 			else {
 				//check crc
-				if (!crc32(temp, frameLength + 4)) {//crc faild
+				if (crc32(temp, frameLength) != 0) {//crc faild
 					//send nak
 					temp[0] = FRAME_NAK;//if the 2nd byte is error, it may sends false nak, but it doesn't matter
 					mySendFrame(temp, 2);
+					printf("crc check failed\n");
 				}
 				else {
-					if (isInBuffer(senderLeft, senderRight, temp[1])) {//sequence is in buffer
+					if (isInBuffer(senderLeft, senderRight, temp[1], false)) {//sequence is in buffer
 						if (temp[0] == FRAME_ACK) {//if it's an ack frame
-							sender[temp[1] % bufferSize].ackArrived = true;
-							stop_timer(temp[1] % bufferSize);//stop timmer
+							if (isInBuffer(lastAck, senderRight, temp[1], false))
+								lastAck = temp[1];
 							//else do noting
+							break;
 						}
 						else if (temp[0] == FRAME_NAK) {//if it's a nak frame
 							//retranmit
 							temp[0] = 1;
-							memcpy(temp + 3, sender[temp[1]].data, sender[temp[1]].length * sizeof(unsigned char));
-							mySendFrame(temp, sender[temp[1]].length + 3);
+							memcpy(temp + 3, sender[temp[1] % bufferSize].data, sender[temp[1] % bufferSize].length * sizeof(unsigned char));
+							mySendFrame(temp, sender[temp[1] % bufferSize].length + 3);
+							break;
 						}
 					}
-					else if (isInBuffer(receiverLeft, (receiverLeft + bufferSize - 1) % windowSize, temp[1])){
+					if (isInBuffer(receiverLeft, (receiverLeft + bufferSize) % windowSize, temp[1], true)){
 						if (temp[0] == FRAME_DATA) {//if it's a data frame
-							receiver[temp[1]].ackArrived = 1;
-							memcpy(receiver[temp[1]].data, (void*)(temp[3]), (frameLength - 7) * sizeof(unsigned char));
-							receiver[temp[1]].length = frameLength - 7;
+							
+							receiver[temp[1] % bufferSize].frameArrived = true;
+							if (isInBuffer(lastAck, senderRight, temp[2], false))
+								lastAck = temp[2];
+
+							receiver[temp[1] % bufferSize].length = frameLength - 7;
+							for (int i = 0; i < frameLength - 7; i++) {
+								receiver[temp[1] % bufferSize].data[i] = temp[3 + i];
+							}
+							//memcpy(receiver[temp[1] % bufferSize].data, (void*)(temp + 3), (frameLength - 7) * sizeof(unsigned char));
+							dbg_event("receiver[%d]:\n", temp[1] % bufferSize);
+							for (int j = 0; j < receiver[temp[1] % bufferSize].length; j++)
+								dbg_event("%d ", receiver[temp[1] % bufferSize].data[j]);
+							
 						}
 					}
 
 				}
 
 			}
+			break;
+		case DATA_TIMEOUT:
+			//just retransmit the frame
+			//build the frame
+			temp[0] = FRAME_DATA;
+			temp[1] = (senderLeft + eventArgs) % windowSize;
+			memcpy((void*)(temp + 3), sender[(senderLeft + eventArgs) % bufferSize].data, sender[(senderLeft + eventArgs) % bufferSize].length * sizeof(unsigned char));
+			//transmit
+			mySendFrame(temp, sender[(senderLeft + eventArgs) % bufferSize].length + 3);
+			break;
+		case ACK_TIMEOUT:
+			//just send an ack
+			temp[0] = FRAME_ACK;
+			mySendFrame(temp, 2);
+			break;
+		}
+		
+
+		//sliding the sender window
+		{
+			int i = senderLeft;
+			while (isInBuffer(senderLeft, senderRight, i, false)) {
+				if (sender[i % bufferSize].hasSent == false){
+					if (isPhysicalLayerReady == 1 || isPhysicalLayerReady == -1) {
+						//build the frame
+						temp[0] = FRAME_DATA;
+						temp[1] = i % windowSize;
+						memcpy((void*)(temp + 3), sender[i % bufferSize].data, sender[i % bufferSize].length * sizeof(unsigned char));
+						//transmit
+						mySendFrame(temp, sender[i % bufferSize].length + 3);
+						sender[i % bufferSize].hasSent = true;
+						isPhysicalLayerReady = 0;
+					}
+					break;
+				}
+				i = (i + 1) % windowSize;
+			}
+		}
+		while (isInBuffer(senderLeft, senderRight, lastAck, false)) {//·â×°º¯Êý
+				sender[senderLeft % bufferSize].hasSent = false;
+				stop_timer(senderLeft % bufferSize);
+				senderLeft = (senderLeft + 1) % windowSize;
+		}
+#ifdef windowdebug
+		printf("sl=%d,sr=%d\n", senderLeft, senderRight);
+#endif
+		//enable network layer
+		if (!(((senderRight > senderLeft) && (senderRight - senderLeft == bufferSize)) || (senderRight < senderLeft) && (windowSize - senderLeft + senderRight == bufferSize)) && !isNetworkEnabled) {
+			enable_network_layer();
+			isNetworkEnabled = true;
 		}
 
+		//sliding the receiver window
+		for (int i = 0; i < bufferSize; i++) {
+			if (receiver[(receiverLeft + i) % bufferSize].frameArrived) {
+#ifdef debug
+				printf("put=\t%d\n", *(short*)(receiver[(receiverLeft + i) % bufferSize].data));
+#endif
+				dbg_event("\nput %d\n", (receiverLeft + i) % bufferSize);
+				put_packet(receiver[(receiverLeft + i) % bufferSize].data, receiver[(receiverLeft + i) % bufferSize].length);
+				receiver[(receiverLeft + i) % bufferSize].frameArrived = false;
+			}
+			else {
+				receiverLeft = (receiverLeft + i) % windowSize;
+				break;
+			}
+		}
+#ifdef windowdebug
+		printf("rl=%d,rr=%d\n", receiverLeft, (receiverLeft + bufferSize - 1) % windowSize);
+		printf("lastack=%d\n\n", lastAck);
+#endif
 	}
 }
